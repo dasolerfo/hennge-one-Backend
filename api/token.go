e93b111278c128cd6d8df460de130862c62c7d97
Daniel Soler <dasolerfo@gmail.com>
2025-08-21 20:03:13 +0200

    Token Request Response and Validation structure set

--- api/token.go
+++ api/token.go
@@ -6,6 +6,37 @@ func TokenGetHandler(c *gin.Context) {
 
 }
 
-func TokenPostHandler(c *gin.Context) {
+type TokenPostHandlerRequest struct {
+	GrantType   string `form:"grant_type" binding:"required"`
+	ClientID    string `form:"client_id" binding:"required"`
+	RedirectUri string `form:"redirect_uri" binding:"required"`
+	Code        string `form:"code" binding:"required"`
+}
+type TokenPostHandlerResponse struct {
+	AccessToken  string `json:"access_token"`
+	TockenType   string `json:"token_type"`
+	RefreshToken string `json:"refresh_token"`
+	ExpiresIn    int64  `json:"expires_in"`
+	IdToken      string `json:"id_token"`
+}
+
+func (server *Server) TokenPostHandler(c *gin.Context) {
+	var req TokenPostHandlerRequest
+	if err := c.ShouldBind(&req); err != nil {
+		// TODO: Afegir la validació dels errors correctament
+	}
+	authCode, err := server.store.GetAuthCode(c.Request.Context(), req.Code)
+	if err != nil {
+		// Validar correctament l'error
+	}
+	if authCode.ClientID != req.ClientID {
+		// Validar correctament l'error
+	}
+	if req.RedirectUri != "" && authCode.RedirectUri != req.RedirectUri {
+		// Validar correctamnet l'error
+	}
+	if authCode.Scope.String != "openid" {
+		//Validar correctament l'error
+	}
 
 }
